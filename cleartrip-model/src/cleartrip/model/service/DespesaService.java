4a3c9a56c2665234fcc6a9b5949a3f081b15fc14
Fábio <Fbio@Fbio-PC>
2014-09-11 06:25:35 -0300

    Término do validate for create do DespesaService

--- cleartrip-model/src/cleartrip/model/service/DespesaService.java
+++ cleartrip-model/src/cleartrip/model/service/DespesaService.java
@@ -5,6 +5,8 @@ import cleartrip.model.base.service.BaseDespesaService;
 import cleartrip.model.dao.DespesaDAO;
 import cleartrip.model.pojo.Despesa;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -92,10 +94,49 @@ public class DespesaService implements BaseDespesaService {
         }
     }
 
-    //AINDA TEM QUE FAZER AQUI
+    
     @Override
     public Map<String, String> validateForCreate(Map<String, Object> properties) throws Exception {
+        Connection conn = ConnectionManager.getInstance().getConnection();
         Map<String, String> erros = new HashMap<String, String>();
+        if (properties != null) {
+            
+            Long viagemFk = (Long) properties.get("viagem_fk");
+            if (viagemFk == null) {
+                erros.put("viagem_fk", "Campo obrigatório!");
+            }
+            
+            Long categoriaFk = (Long) properties.get("categoria_fk");
+            if (viagemFk == null) {
+                erros.put("categoria_fk", "Campo obrigatório!");
+            }
+            
+            byte[] comprovate = (byte[]) properties.get("comprovate");
+            if (comprovate == null) {
+                erros.put("comprovate", "Campo obrigatório!");
+            }
+            
+            int valor = (int) properties.get("valor");
+            if (valor == 0) {
+                erros.put("valor", "Campo obrigatório!");
+            }
+            
+            Date dataCompra = (Date) properties.get("data_compra");
+            if (dataCompra == null) {
+                erros.put("data_compra", "Campo obrigatório!");
+            }
+            
+            Time horaCompra = (Time) properties.get("hora_compra");
+            if (horaCompra == null) {
+                erros.put("hora_compra", "Campo obrigatório!");
+            }
+            
+            int valorRealAutorizado = (int) properties.get("valor_autorizado");
+            if (valorRealAutorizado == 0) {
+                erros.put("valor_autorizado", "Campo obrigatório!");
+            }
+            
+        }
         return erros;
     }
 
